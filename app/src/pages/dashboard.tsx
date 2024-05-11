afb9e1884f6cdb1dab689aca932ccb38cdc5ecde
Kengo Yamashita <yamashita-kenngo@outlook.jp>
2024-05-11 21:32:57 +0900

    feat: add menu in dashboard (#6)

--- app/src/pages/dashboard.tsx
+++ app/src/pages/dashboard.tsx
@@ -8,20 +8,42 @@ const Dashboard = () => {
   const navigate = useNavigate();
 
   createEffect(async () => {
-    // check if user is signed in
     if (!(await isVerifiedAccount())) {
       navigate("/403");
     }
   });
 
   return (
-    // tailwindcss classes
     <div class="bg-gray-100 h-screen">
-      <h1 class="text-2xl font-bold text-center pt-10">Dashboard</h1>
-      <div class="flex justify-center items-center h-full">
-        <div class="bg-white p-5 rounded-lg shadow-lg">
-          <h2 class="text-xl font-semibold">Welcome to your dashboard!</h2>
-          <p class="text-gray-500">You have successfully logged in.</p>
+      <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold leading-tight text-gray-900">Dashboard</h1>
+          <div class="flex items-center justify-end">
+            <div class="ml-4 flex items-center md:ml-6">
+              {/* pulldown menu select */}
+              <select class="block appearance-none bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                <option>🏘️Tenant Info</option>
+                <option>🪪Account Info</option>
+                <option>🛫Product Info</option>
+                <option>⚙️Settings</option>
+              </select>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded">
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div class="px-4 py-6 sm:px-0">
+          <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
+            <div class="flex flex-col items-center justify-center h-full">
+              <h2 class="text-2xl font-bold text-gray-900">Welcome to Dashboard</h2>
+              <button class="mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                Register Tenant
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
